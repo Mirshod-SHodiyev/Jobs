5025bbf913212e08b3fe1f1d967cc780977d3b38
shodiyevmirshod <shodiyevmirshod43@gmail.com>
2025-04-01 17:25:29 +0500

    composer.lock yangilandi

--- app/Commands/BotCommand.php
+++ app/Commands/BotCommand.php
@@ -12,6 +12,7 @@ class BotCommand extends Command
     public function handle()
     {
         $this->info('Bot started successfully!');
-        // Bu yerga bot logikasini yozing
+        
+        
     }
 }
