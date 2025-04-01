94868a39dea571f64377e147ce579ba2d7c65b5c
shodiyevmirshod <shodiyevmirshod43@gmail.com>
2025-04-01 15:19:35 +0500

    add sayt seting

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Job.uz🇺🇿</title>
+    <meta name="description" content="Job.uz🇺🇿 – O'zbekistondagi eng yaxshi ish qidirish sayti. Eng so'nggi vakansiyalarni toping!">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="icon" type="image/x-icon" href="/favicon.ico">
