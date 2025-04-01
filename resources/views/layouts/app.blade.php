81436ab9a91728adc5b34c4f4a209a0d2d2e639b
shodiyevmirshod <shodiyevmirshod43@gmail.com>
2025-04-01 20:24:28 +0500

    yangilandi

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Job.uz🇺🇿</title>
-    <meta name="description" content="Job.uz🇺🇿 – O'zbekistondagi eng yaxshi ish qidirish sayti. Eng so'nggi vakansiyalarni toping!">
+    <title> Job.uz🇺🇿 - Ish Qidirish va Topish | O‘zbekistondagi Eng Yaxshi Platforma</title>
+    <meta name="description" content="Job.uz🇺🇿 - Ish qidirish va topish oson! 1000+ vakansiya, 500+ kompaniya, 10,000+ foydalanuvchi biz bilan ish topmoqda.">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="icon" type="image/x-icon" href="/favicon.ico">
